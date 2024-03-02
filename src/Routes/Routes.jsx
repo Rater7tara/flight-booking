d1257ec44e5cd15a1ca95d6fcbce23e7bf5ebb05
Rater7tara <nowshinakteremu005@gmail.com>
2024-03-02 16:25:05 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import {
+    Navigate,
+    createBrowserRouter,
+} from "react-router-dom";
+import App from "../App";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App></App>,
+    }
+])
